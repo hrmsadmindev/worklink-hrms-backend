b86476dcb9f910743fd9e30037f7e8bfa0289c14
hrmsadmindev <hrmssolutions1@gmail.com>
2025-08-03 22:40:59 +0530

    Removed demo credentials comments

--- src/main/java/com/worklink/hrms/HrmsApplication.java
+++ src/main/java/com/worklink/hrms/HrmsApplication.java
@@ -19,9 +19,5 @@ public class HrmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(HrmsApplication.class, args);
         System.out.println("HRMS Application Started Successfully!");
-//        System.out.println("Access H2 Console: http://localhost:8080/api/h2-console");
-//        System.out.println("Admin Login: admin@company.com / admin123");
-//        System.out.println("Manager Login: manager@company.com / manager123");
-//        System.out.println("Employee Login: employee@company.com / employee123");
     }
 }
